ae8340130bf701420296ae5fe364df0342718a31
PalashDevOps <palashchimurkar75@gmail.com>
2024-01-07 19:20:50 +0530

    added Hello Wolrd.py

---
+++ helloworld.py
@@ -0,0 +1,19 @@
+num = 1
+string_value = "I am a String Value"
+char = 'A'
+float_num = 9.99
+booleanval = "true"
+variable = "Hello World"
+print(type(num))
+print(type(string_value))
+print(type(float_num))
+print(type(booleanval))
+print(type(variable))
+print(type(char))
+
+print(num)
+print(string_value)
+print(float_num)
+print(booleanval)
+print(variable)
+print(char)
